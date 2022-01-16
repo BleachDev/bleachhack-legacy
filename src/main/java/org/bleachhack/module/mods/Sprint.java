e19cd127cfdc3ebb06ccc84a12e5cc2c503ca2cb
milo <66573539+unfidelity@users.noreply.github.com>
2022-01-16 03:02:43 -0500

    s[romt check

--- src/main/java/org/bleachhack/module/mods/Sprint.java
+++ src/main/java/org/bleachhack/module/mods/Sprint.java
@@ -4,17 +4,26 @@ import org.bleachhack.event.events.EventTick;
 import org.bleachhack.eventbus.BleachSubscribe;
 import org.bleachhack.module.Module;
 import org.bleachhack.module.ModuleCategory;
+import org.bleachhack.module.ModuleManager;
+import org.bleachhack.setting.module.SettingToggle;
 
 public class Sprint extends Module {
 
     public Sprint(){
 
-        super("Sprint", KEY_UNBOUND, ModuleCategory.MOVEMENT, "Automatically Toggles Sprint");
+        super("Sprint", KEY_UNBOUND, ModuleCategory.MOVEMENT, "Automatically Toggles Sprint",
+                new SettingToggle("Hunger Check", true).withDesc("Checks if you can actually sprint (unfucks running)")
+        );
 
     }
 
     @BleachSubscribe
     public void onTick(EventTick event) {
+
+        if(mc.field_3805.getHungerManager().getFoodLevel() <= 6 && getSetting(0).asToggle().getState()){ //checks hungerlevel so it stops sprinting if under 3 bars of hunger
+            return;
+        }
+
         if(!mc.field_3805.isSprinting() && !mc.field_3805.isSneaking() && mc.field_3805.forwardSpeed > 0.0f){
             mc.field_3805.setSprinting(true);
         }
