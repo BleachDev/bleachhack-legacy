ffd5848f6c359fd64ae4c8156483bcd9ff09ee5c
milo <66573539+unfidelity@users.noreply.github.com>
2022-01-16 01:53:52 -0500

    spront

---
+++ src/main/java/org/bleachhack/module/mods/Velocity.java
@@ -0,0 +1,16 @@
+package org.bleachhack.module.mods;
+
+import org.bleachhack.module.ModuleCategory;
+
+import static org.bleachhack.module.Module.KEY_UNBOUND;
+
+public class Velocity extends Module {
+
+    public Velocity(){
+
+        super("Sprint", KEY_UNBOUND, ModuleCategory.MOVEMENT, "No Velocity");
+
+    }
+
+
+}
